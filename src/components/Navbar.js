4173f1ce18c0c799e1bf0d1d738fc763377eda21
Ethan Russon <ethanrusson@Ethans-MacBook-Pro-2.local>
2021-03-09 19:59:31 -0700

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -12,85 +10,38 @@ const Navbar = class extends React.Component {
     }
   }
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
-          : this.setState({
-              navBarActiveClass: '',
-            })
-      }
-    )
-  }
-
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
-          </div>
+      <header>
+        <div class="top-bar">
+          <p>idaho falls bridal &amp; prom<span class="mobile-hidden"> - featuring elizabeth cooper design</span></p>
         </div>
-      </nav>
+        <nav
+          className="navbar is-transparent"
+          role="navigation"
+          aria-label="main-navigation"
+        >
+          <div className="desktop-nav">
+            <Link to="/" title="Logo"></Link>
+            <Link to="/bridal">Bridal</Link>
+            <Link to="/prom">Prom</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+          <div className="menu-icon" id="menu-icon">
+            <p>Menu</p>
+            <span id="icon-x"></span>
+          </div>
+          <div class="mobile-nav" id="mobile-nav">
+            <Link to="/" title="Logo"></Link>
+            <Link to="/bridal">Bridal</Link>
+            <Link to="/prom">Prom</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <a href="https://www.google.com/maps/place/Tangled+in+Tulle+Bridal+%26+Prom/@43.498405,-112.0017527,17z/data=!3m1!4b1!4m5!3m4!1s0x53545e8e83e8c13b:0x7a5be306401dfcde!8m2!3d43.498405!4d-111.999564">Get Directions</a>
+          </div>
+        </nav>
+      </header>
     )
   }
 }
